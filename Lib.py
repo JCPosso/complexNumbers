b3c3119033315a4ad9e9ac3c91d5d321e0e649a0
JCPosso <JCPosso@users.noreply.github.com>
2020-01-17 22:33:11 -0500

    Update Lib.py
    none

--- Lib.py
+++ Lib.py
@@ -34,7 +34,7 @@ def cartesianToPolar(a):
         return ("NOT possible to convert!")
     else :
         return (fhi,ang)
-        
+        //jesusuuit
   def fase(a):
     return math.atan2(a[1],a[0])
 
