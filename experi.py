3773545aa9f2ac3fbe64da95227dfac5e9b85073
JCPosso <44146884+JCPosso@users.noreply.github.com>
2020-02-19 21:33:30 -0500

    Delete experi.py

--- experi.py
+++
@@ -1,9 +0,0 @@
-import math;
-from LibVectoresyMatricesComplejas import *
-class experimentos(matriz):
-    def marbles(self):
-        m=input("por favor ingrese una matriz cuadrada booleana",end="")
-        if(m.esCuadrada()):
-            m=matriz(m)
-            e0=input("por favor ingrese un estado en forma vectorial",end="")
-            if(len(m.c)):
