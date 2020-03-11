f6e27e1d219b78d4ce1752e44f4a3ab763f6bdda
JCPosso <44146884+JCPosso@users.noreply.github.com>
2020-03-11 18:44:50 -0500

    se añaden nuevos metodos

--- LibVectoresyMatricesComplejas.py
+++ LibVectoresyMatricesComplejas.py
@@ -171,113 +171,12 @@ class matriz:
                 return len(self.c)==len(self.c[0])
         def n_BlancosOptimo(n,n2):
                 return n+( ((n//2)+1)*(n2-1))
-        
         def isDoublyStochastic(self):
                 vPrueba=matriz.iniciar(len(self.c),1)
                 for i in range(len(self.c)):
                         vPrueba.c[i][0]=complejo(1,0)
                         
                 return self.alcanceSobre(vPrueba)==vPrueba
-        
-        def comprobar_Parametros_Multi(self,n_blancosPorRendija,b_central,n_rendijas,n_blancos):
-                try:
-                        if(not(n_blancosPorRendija+( b_central*(n_rendijas-1) )== n_blancos)):
-                                print(n_blancosPorRendija+( b_central*(n_rendijas-1) ), n_blancos)
-                                raise ValueError("El numero de blancos por rendija no es compatible con el numero de blancos , por favor intente nuevamente")
-                except ValueError:
-                        print("Failed :")
-                        raise
-        def fill_ways(self,n_blancosPorRendija,n_rendijas,n_blancos , b_central,vector):
-                rendija=1
-                k=int(n_rendijas+b_central)
-                flag=False
-                for i in range( len(vector.c)):
-                        if(vector.c[i][0].img!=0):
-                                flag=True
-                                break
-                #primera parte de llenado
-                for i in range( 1, n_rendijas+1):
-                        if(flag):
-                                self.c[i][0].real=1/math.sqrt(n_rendijas)
-                        else:
-                                self.c[i][0].real=1/(n_rendijas)
-                #segunda parte de llenado       
-                while(k<len(self.c)):
-                        self.c[k][rendija]=vector.c[b_central-1][0]
-                        cont=1
-                        while(cont<n_blancosPorRendija):
-                                if(cont%2!=0):
-                                        sig=(cont+1)//2                
-                                        self.c[k-sig][rendija]=vector.c[(b_central-1) -sig ][0]
-                                        cont+=1
-                                else:
-                                        sig= cont//2
-                                        self.c[k+sig][rendija]=vector.c[(b_central-1) +sig ][0]
-                                        cont+=1
-                        k+=b_central
-                        rendija+=1
-                #tercera parte de llenado       
-                for i in range(n_blancos):
-                        self.c[len(self.c)-1-i][len(self.c)-1-i]=complejo(1,0)
-                        
-                return self
-        def matriz_multirendija(self,n_rendijas,n_blancos,vector_prob):
-                # si me ingresa un numero general de probabilidades  en vez de un vector , se convierte a vector
-                if (isinstance(vector_prob,float)):
-                        n_blancosPorRendija=int(vector_prob**(-1))
-                        vector_prob=    matriz.iniciar(int(vector_prob**(-1)),1)
-                        for  i in range(n_blancosPorRendija):
-                                vector_prob.c[i][0]=complejo(1/n_blancosPorRendija,0)
-
-                n_blancosPorRendija = len(vector_prob.c)
-                tam_matriz=n_rendijas+n_blancos+1
-                ways= matriz.iniciar(tam_matriz,tam_matriz)
-                b_central=int((n_blancosPorRendija//2)+1)
-
-                matriz.comprobar_Parametros_Multi(None,n_blancosPorRendija,b_central,n_rendijas,n_blancos)
-                
-                return ways.fill_ways(n_blancosPorRendija ,n_rendijas,n_blancos,b_central,vector_prob)
-        
-        def experimento_multirendija_cuantico(self,n_rendijas,n_blancos,vector_prob):
-                
-                m=matriz.matriz_multirendija(None,n_rendijas,n_blancos,vector_prob)
-                m= m.potencia(2)
-                #print(m)
-                for i in range(len(m.c)):
-                        for j in range(len(m.c[0])):
-                                m.c[i][j]=complejo(m.c[i][j].modulo()*m.c[i][j].modulo(),0)              
-                v= matriz.iniciar(len(m.c),1)
-                v.c[0][0]=complejo(1,0)
-                stat= m.alcanceSobre(v)
-                #matriz.graficar(stat)
-                return m,stat
-                
-                
-        def experimento_multirendija_probabilistico(self,n_rendijas,n_blancos,vector_prob):
-                m=matriz.matriz_multirendija(None,n_rendijas,n_blancos,vector_prob)
-                v= matriz.iniciar(len(m.c),1)
-                v.c[0][0]=complejo(1,0)
-                stat= m.state(v,2)
-                #matriz.graficar(stat)
-                return m,stat
-                
-        def graficar(stat):
-                axis_x= []
-                axis_y=[]
-                for i in range( len(stat.c)):
-                        axis_x.append(i)
-                for j in range( len(stat.c)):
-                        axis_y.append(stat.c[j][0].real)
-                
-                fig = plt.figure(u'Vectores de resultado') # Figure
-                ax = fig.add_subplot(111) # Axes
-                datos = axis_y
-                xx = range(len(datos))
-                nombres=axis_x
-                ax.bar(xx, datos, width=0.8, align='center')
-                ax.set_xticks(xx)
-                ax.set_xticklabels(nombres)
-                plt.show()
         def __str__(self):
                 s=""
                 for j in range(len(self.c)):
