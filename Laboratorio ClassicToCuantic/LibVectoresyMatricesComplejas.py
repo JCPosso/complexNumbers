2c4d1b4b05a27b3086d252babfb1d10abb44c2d7
JCPosso <JCPosso@users.noreply.github.com>
2020-03-11 23:27:42 -0500

    Version final

---
+++ Laboratorio ClassicToCuantic/LibVectoresyMatricesComplejas.py
@@ -0,0 +1,187 @@
+#-*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import math;
+from LibreriaNumerosComplejos import *
+"""Librería computación Cuántica: Números complejos
+   librería para hacer operaciones entre vectores y matrices de  complejos.
+   @author (Juan Camilo Posso G.) 
+   @version (1.0 or 16/01/2020)rr
+"""
+class matriz:
+        def __init__(self,c):
+                self.c=c
+                for j in range(len(self.c)):
+                        for k in range(len(self.c[0])):
+                                if(not(isinstance(self.c[j][k],complejo))):
+                                        self.c[j][k]=complejo(self.c[j][k][0],self.c[j][k][1])
+        def __eq__(self, other):
+                """Override"""
+                if(len(self.c[0])==len(other.c[0])and len(self.c)==len(other.c)):
+                        for j in range(len(self.c)):
+                                for k in range(len(self.c[0])):
+                                        if isinstance(other.c[j][k], complejo):
+                                                if(round(self.c[j][k].real,4) != round(other.c[j][k].real,4) or round(self.c[j][k].img,4) != round(other.c[j][k].img,4)):
+                                                        return False
+                        return True
+                return False
+        def iniciar(f,c):
+                m=[]
+                for i in range(f):
+                        m.append([ [0,0] ]*c)
+                return matriz(m)
+        def unitaria(c):
+                m=[]
+                temp=matriz.iniciar(c,c)
+                for i in range(c):
+                        temp.c[i][i]=complejo(1,0)
+                return temp
+        def trace(C):
+                suma=complejo(0,0)
+                for j in range(len(C.c[0])):
+                        suma=suma.suma(C.c[j][j])
+                return suma
+        def instanciar(self):
+                v2=matriz.iniciar(len(self.c),len(self.c[0]))
+                for j in range(len(self.c)):
+                        for k in range(len(self.c[0])):
+                                v2.c[j][k]=self.c[j][k]
+                return v2
+        
+        def suma(self,W):
+                m=len(self.c);n=len(self.c[0]);n2=len(W.c);p=len(W.c[0])
+                if(m!=n2 or n!=p):return( "Las matrices no tienen el tamaño apropiado")
+                res=matriz.iniciar(len(self.c),len(self.c[0]))
+                for j in range(len(self.c)):
+                        for k in range(len(self.c[0])):
+                                res.c[j][k]=(self.c[j][k]).suma(W.c[j][k])
+                return res
+        
+        def inversa(self):
+                for j in range(len(self.c)):
+                        for k in range(len(self.c[0])):
+                                self.c[j][k]=(self.c[j][k]).multiplica(complejo(-1,0))
+                return self
+
+        def multiplica(self,W):
+                m=len(self.c);n=len(self.c[0]);n2=len(W.c);p=len(W.c[0])
+                if(n!=n2): return "Las matrices no tienen el tamaño apropiado"
+                res=matriz.iniciar(len(self.c),len(W.c[0]))
+                for j in range(m):
+                        for k in range(p):
+                                cont=complejo(0,0)
+                                for h in range(n):
+                                        cont=cont.suma( (self.c[j][h]).multiplica(W.c[h][k]) )
+                                res.c[j][k]=cont
+                return res
+        def multiplicaEscalar(self,e):
+                if(isinstance(self,complejo)):temp=e;e=self;self=temp
+                res=matriz.iniciar(len(self.c),len(self.c[0]))
+                for j in range(len(self.c)):
+                        for k in range(len(self.c[0])):
+                                res.c[j][k]=(self.c[j][k]).multiplica(e)
+                return res
+        
+        def transpuesta(self):
+                m=matriz.iniciar(len(self.c[0]),len(self.c))
+                for j in range(len(self.c[0])):
+                        for k in range(len(self.c)):
+                                m.c[j][k]=self.c[k][j]
+                self.c=m.c
+                return self
+                
+        def conjugada(self):
+                for j in range(len(self.c)):
+                        for k in range(len(self.c[0])):
+                                self.c[j][k]=self.c[j][k].conjugado()
+                return self
+        
+        def adjunta(self):
+                self=(self.transpuesta()).conjugada()
+                return self
+        
+        def alcanceSobre(self,vector):
+                if(len(vector.c[0])==1 ):
+                        res=matriz.iniciar(len(vector.c),1)
+                        for j in range(len(self.c)):
+                                for k in range(len(self.c[0])):
+                                        res.c[j][0] =res.c[j][0].suma( self.c[j][k].multiplica(vector.c[k][0]) )
+                        return res
+                return "no ingresó un vector, intente nuevamente!"
+        
+        def productoInterno(self,W):
+                producto= (self.adjunta() ).multiplica(W)
+                if( len(self.c[0])==len(W.c[0])==1):
+                        return producto
+                return producto.trace()
+
+        def isHermitian(self):
+                n=matriz.instanciar(self)
+                for j in range(len(self.c)):
+                            for k in range(len(self.c[0])):
+                                    if( self.c[j][k]!=n.c[k][j].conjugado()):
+                                            return False
+                return True
+        
+        def isUnitary(self):
+                n=matriz.instanciar(self)
+                if(len(self.c[0])==len(self.c)):
+                        if( self.multiplica(n.adjunta())==(self.adjunta().multiplica(n) )):
+                                return True
+                return False
+
+        def norma(self):
+                m= matriz(self.c)
+                return round(math.sqrt(int(str(self.productoInterno(m)))),2)
+
+        def distancia(self,v2):
+                return (self.suma( v2.inversa()).norma( ))
+
+        
+        def productoTensor(self,B):
+                f=len(B.c)*len(self.c)
+                c=len(self.c[0])*len(B.c[0])
+                matrix=matriz.iniciar(f,c)
+                o=0;p=0
+                for j in range(len(matrix.c)):
+                        for k in range(len(matrix.c[0])):
+                                matrix.c[j][k]=self.c[j//len(B.c)][k//len(B.c[0])].multiplica(B.c[j%len(B.c)][k%len(B.c[0])])
+                return matrix
+        def potencia(self,n):
+                if(n==1):
+                        return self
+                else:
+                        if( (n%2)==0):
+                                return (self.potencia(n//2)).multiplica(self.potencia(n//2))
+                        else:
+                                return (self.potencia(n//2).multiplica(self.potencia(n//2))).multiplica(self)
+
+        def state(self,v,times):
+                if(times==0):return self
+                return (self.potencia(times)).alcanceSobre(v)
+        
+        def combinar(m1,m2,v1,v2,n):
+                if (n==0):
+                        e1=v1
+                        e2=v2
+                else:
+                        e1=m1.potencia(n).multiplica(v1)
+                        e2=m2.potencia(n).multiplica(v2)
+                return e1.productoTensor(e2)
+        def cuadrada(self):
+                return len(self.c)==len(self.c[0])
+        def n_BlancosOptimo(n,n2):
+                return n+( ((n//2)+1)*(n2-1))
+        def isDoublyStochastic(self):
+                vPrueba=matriz.iniciar(len(self.c),1)
+                for i in range(len(self.c)):
+                        vPrueba.c[i][0]=complejo(1,0)
+                        
+                return self.alcanceSobre(vPrueba)==vPrueba
+        def __str__(self):
+                s=""
+                for j in range(len(self.c)):
+                        s+="[ "
+                        for k in range(len(self.c[0])):
+                                s+=str(self.c[j][k])+" "
+                        s+=']\n'
+                return s
---
+++ Laboratorio ClassicToCuantic/LibreriaNumerosComplejos.py
@@ -0,0 +1,122 @@
+import math;
+class complejo:
+        '''
+        Libreria de Numeros complejos de argumentos r,i en donde r e i son las componentes cartesianes
+        real e imaginaria , respectivamente.Todos los atributos se daran tanto en pares de  complejos
+        o de forma unaria.
+        '''
+        def __init__(self,r,i):
+                self.real= r
+                self.img= i
+        def __eq__(self, other):
+                """Override"""
+                if isinstance(other, complejo):
+                        return( round(self.real,2) == round(other.real,2) and round(self.img,2) == round(other.img,2))
+                return False
+        def suma(self,b):
+                '''
+                Retorna la suma de dos complejos.
+
+                Excepciones:
+                si alguno de los dos numeros no es un complejo
+                '''
+                self.compare(b)
+                real = self.real + b.real
+                img  = self.img + b.img
+                return complejo(real,img)
+        def resta(self,b):
+                '''
+                Retorna la resta de dos complejos.
+
+                Excepciones:
+                si alguno de los dos numeros no es un complejo
+                '''
+                self.compare(b)
+                real = self.real - b.real
+                img  = self.img - b.img
+                return complejo(real,img)
+        def multiplica(self,b):
+                '''
+                Retorna la multiplicación de dos complejos.
+
+                Excepciones:
+                Si alguno de los dos numeros no es un complejo
+                '''
+                self.compare(b)
+                real=self.real*b.real - self.img*b.img
+                img= self.real*b.img + b.real*self.img
+                return complejo(real,img)
+        def divide(self,b):
+                '''
+                Retorna la división de dos complejos.
+                Parte real e imaginaria redondeados a dos decimales.
+
+                Excepciones:
+                si alguno de los dos numeros no es un complejo
+                Si el divisor es cero
+                '''
+                self.compare(b)
+                try:
+                        x=(self.real*b.real+self.img*b.img)
+                        y=(b.real*self.img-self.real*b.img)
+                        divisor=( b.real**2)+ (b.img**2)
+                        return complejo(round(x/divisor,2),round(y/divisor,2))
+                except ZeroDivisionError as m:
+                        raise Exception("Division por 0!!!")
+        def modulo(self):
+                '''
+                Retorna la módulo del complejo.
+                Redondeado a dos decimales.
+                '''
+                mod= math.sqrt(self.real**2+self.img**2)
+                return  mod
+        def conjugado(self):
+                '''
+                Retorna conjugado del complejo.
+                '''
+                return complejo(self.real,self.img*-1)
+        def aPolar(self):
+                '''
+                Retorna el numero en coordenadas polares.
+                
+                Excepciones:
+                Si la Parte real es cero
+                '''
+                try:
+                        fhi=round(math.sqrt(self.real**2+self.img**2),2)
+                        ang=round(math.atan2(self.img,self.real),2)
+                        return complejo(fhi,ang)
+                except ZeroDivisionError as m:
+                        raise Exception("Parte Real es Cero!!!")
+        def fase(self):
+                '''
+                Retorna la Fase del número.
+                
+                Excepciones:
+                Si la Parte real es cero
+                '''
+                try:
+                        res= round(math.atan2(self.img,self.real),2)
+                        return res
+                except ZeroDivisionError as m:
+                        raise Exception("Parte Real es Cero!!!")
+        def compare(self,r):
+                if (not (isinstance(self,complejo) and isinstance(r,complejo)) ) :
+                        raise Exception("ERROR:Favor Ingresar un formato válido")
+
+        def __str__(self):
+                s1=str(self.real)
+                if(self.img!=1 and self.img!=-1 ):
+                        s2=str(self.img)+"i"
+                else:
+                        if (self.img==-1):s2="-i"
+                        else :s2="i"
+
+                if(self.real==0):
+                        if (self.img==0):return '0'
+                        else            :return s2
+                else:
+                        
+                        if (self.img>0):return s1+"+"+s2
+                        if (self.img<0):return s1+s2
+                return s1
